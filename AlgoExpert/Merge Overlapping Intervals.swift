0f68ba90d21203a7910d3aad90ae206368944ddc
Benson_Lin <benson_lin@books.com.tw>
2021-04-08 18:21:27 +0800

    Merge Overlapping Intervals

---
+++ AlgoExpert/Merge Overlapping Intervals.swift
@@ -0,0 +1,39 @@
+//
+//  Merge Overlapping Intervals.swift
+//  AlgoExpert
+//
+//  Created by 林翌埕-20001107 on 2021/4/7.
+//
+
+import Foundation
+
+extension Program {
+    static func mergeOverlappingIntervals(_ intervals: [[Int]]) -> [[Int]] {
+        var result: [[Int]] = []
+        var currentIdx = 0
+        let sortedIntervals = intervals.sorted(by: { $0[0] < $1[0] })
+        
+        for idx in 0 ..< sortedIntervals.count {
+            if result.isEmpty {
+                result.append(sortedIntervals[idx])
+                continue
+            }
+            
+            if sortedIntervals[idx][0] <= result[currentIdx][1] {
+                // right bound overlapping
+                result[currentIdx][1] = max(sortedIntervals[idx][1], result[currentIdx][1])
+            } else {
+                result.append(sortedIntervals[idx])
+                currentIdx += 1
+            }
+            
+            if idx == sortedIntervals.count - 1 && result[currentIdx].count == 1 {
+                result[currentIdx][1] = sortedIntervals[idx][1]
+            }
+        }
+        
+        
+        return result
+    }
+}
+
